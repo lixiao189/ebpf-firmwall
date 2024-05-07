315de3de020c5f63bef94046637939cef6012edc
node189 <node189@icloud.com>
2024-05-07 12:03:00 +0800

    refactor: rename the event var

--- src/ebpf-python/main.py
+++ src/ebpf-python/main.py
@@ -6,30 +6,30 @@ bpf = BPF(src_file=b"xdp.c")
 
 
 def receive_callback(ctx, data, size):
-    pkt = bpf["pkt_buffer"].event(data)
+    pkt_event = bpf["pkt_buffer"].event(data)
 
     # extract tcp flags
-    fin_flag = not (pkt.flags & (1 << 7)) == 0
-    syn_flag = not (pkt.flags & (1 << 6)) == 0
-    rst_flag = not (pkt.flags & (1 << 5)) == 0
-    psh_flag = not (pkt.flags & (1 << 4)) == 0
-    ack_flag = not (pkt.flags & (1 << 3)) == 0
-    urg_flag = not (pkt.flags & (1 << 2)) == 0
+    fin_flag = not (pkt_event.flags & (1 << 7)) == 0
+    syn_flag = not (pkt_event.flags & (1 << 6)) == 0
+    rst_flag = not (pkt_event.flags & (1 << 5)) == 0
+    psh_flag = not (pkt_event.flags & (1 << 4)) == 0
+    ack_flag = not (pkt_event.flags & (1 << 3)) == 0
+    urg_flag = not (pkt_event.flags & (1 << 2)) == 0
 
     packet = Packet(
-        src=pkt.src_ip,
-        dst=pkt.dst_ip,
-        proto=pkt.protocol,
-        sport=pkt.src_port,
-        dport=pkt.dst_port,
+        src=pkt_event.src_ip,
+        dst=pkt_event.dst_ip,
+        proto=pkt_event.protocol,
+        sport=pkt_event.src_port,
+        dport=pkt_event.dst_port,
         fin=fin_flag,
         syn=syn_flag,
         rst=rst_flag,
         psh=psh_flag,
         ack=ack_flag,
         urg=urg_flag,
-        icmp_type=pkt.icmp_type,
-        icmp_code=pkt.icmp_code
+        icmp_type=pkt_event.icmp_type,
+        icmp_code=pkt_event.icmp_code
     )
 
 
