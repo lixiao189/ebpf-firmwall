6aa55fdfae0328f98059030583ceadab500e078b
lixiao <node189@icloud.com>
2024-05-15 15:04:53 +0800

    feat: add waf and ml in the system

--- waf/main.go
+++ waf/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
 
 func main() {
-	fmt.Println("hello world")
+	target, err := url.Parse("http://example.com")
+	if err != nil {
+		fmt.Println("Error parsing target URL:", err)
+		return
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(target)
+	d := proxy.Director
+	proxy.Director = func(r *http.Request) {
+		d(r)
+		r.Host = target.Host
+	}
+
+	// Redirect any request to the target URL
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Proxying request to:", r.URL)
+		proxy.ServeHTTP(w, r)
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
